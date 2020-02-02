52f6de82c71b38eab6b132e46bac76f2ccda948b
Shengyu Feng <shengyuf@umich.edu>
2020-02-01 23:16:40 -0800

    dataset

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,21 +5,8 @@ from baselines.ppo2 import ppo2
 from baselines.common.vec_env import VecFrameStack
 from baselines.common.cmd_util import make_vec_env
 
-if __name__ == '__main__':
-    seed = None
-    frame_stack_size = 4
-    num_envs = 50
-    env = make_vec_env("PongNoFrameskip-v4", "atari", num_envs, seed, gamestate = None, reward_scale=1.0)
-    env = VecFrameStack(env, frame_stack_size)
 
-    model = ppo2.learn(
-            env=env,
-            seed=seed,
-            total_timesteps=0,
-            network = "cnn",
-            load_path = "./models/pong_20M_ppo2",
-        )
-
+def collect_states(model, env, limit):
     count = 0
     obs = env.reset()
     states = []
@@ -34,10 +21,37 @@ if __name__ == '__main__':
         obs, rew, done, _ = env.step(actions)
         states.append(obs)
         count+=1
-        if count >= 10:
+        if count >= limit:
             break;
-
+            
     states = np.vstack(states)
-    states = np.random.shuffle(np.moveaxis(states, -1, 1))
-    np.save("toy_states.npy", states)
+    states_sum = states.sum(axis=(1,2,3))
+    states = states[np.where(states_sum!=749607)]
+    return states
+
+if __name__ == '__main__':
+    seed = None
+    frame_stack_size = 4
+    # total <= num_envs*(expert_limit+random_limit) due to noisy states
+    num_envs = 50
+    expert_limit = 2000
+    random_limit = 4000
+    env = make_vec_env("PongNoFrameskip-v4", "atari", num_envs, seed, gamestate = None, reward_scale=1.0)
+    env = VecFrameStack(env, frame_stack_size)
+
+    model = ppo2.learn(
+            env=env,
+            seed=seed,
+            total_timesteps=0,
+            network = "cnn",
+        )
+
+    states = collect_states(model, env, random_limit)
+    model.load("./models/pong_20M_ppo2")
+    states = np.vstack([states, collect_states(model, env, expert_limit)])
+    env.close()
+    states = np.moveaxis(states, -1, 1) # move axis if in pytorch, keep it if in tensorflow   
+    np.random.shuffle(states)
+    print("Generate {} stacked frames".format(len(states)))
+    np.save("ministates.npy", states)
 
